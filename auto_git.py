9f51acafcdb61a9c0af935146bcb9262aa11f5ad
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2022-10-20 00:22:20 +0800

    feat:Thu Oct 20 00:22:20 2022 auto update

--- auto_git.py
+++ auto_git.py
@@ -1,11 +1,14 @@
 from git import Repo
 import os
+import time
 
 dirfile = os.path.abspath('')
 repo = Repo(dirfile)
 
+seconds = time.time()
+local_time = time.ctime(seconds)
 g = repo.git
 g.add("--all")
-g.commit(m='auto update')
+g.commit(m=f'feat:{local_time} auto update')
 g.push()
 print("Successful push!")

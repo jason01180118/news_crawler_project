c40ee8eda4c22959c954e878bd1918cb934a94af
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2022-10-20 00:06:16 +0800

    auto update

--- autogit.py
+++ autogit.py
@@ -1,11 +1,11 @@
 from git import Repo
 import os
 
-dirfile = os.path.abspath('')  # code的文件位置，我默认将其存放在根目录下
+dirfile = os.path.abspath('')
 repo = Repo(dirfile)
 
 g = repo.git
 g.add("--all")
-g.commit("-m auto update")
+g.commit(m='auto update')
 g.push()
 print("Successful push!")

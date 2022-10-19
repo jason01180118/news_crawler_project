ab7bc80786fb0ad927de42e8cbfb8d55f5ddf4f3
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2022-10-19 22:09:26 +0800

    auto update

---
+++ esgnews_web_crawler/split_sqlite.py
@@ -0,0 +1,25 @@
+import sqlite3
+from nltk.tokenize import sent_tokenize
+
+tags = {'governance': 3, 'environment': 1, 'social': 2}
+for tag in tags.keys():
+    conn = sqlite3.connect('./mydb.db')
+    cur = conn.cursor()
+    cur.execute(
+        "select text from REPORTS where ESGtag=?", [tag])
+    texts = cur.fetchall()
+    conn.close()
+    for text in texts:
+        conn = sqlite3.connect('./output.db')
+        cur = conn.cursor()
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS OUTPUTS ('text' TEXT PRIMARY KEY,'ESGtag' TEXT)")
+        sentences = sent_tokenize(str(text)[2:-3].strip().replace('\\n', ''))
+        for sentence in sentences:
+            if sentence != '':
+                cur.execute(
+                    "Insert or ignore into OUTPUTS Values(?,?)", (sentence, tags[tag]))
+        conn.commit()
+        conn.close()
+
+print('done')

cb2ca54de60606887e5bd5f67307601df82b37ce
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2022-11-24 12:00:21 +0800

    feat:Thu Nov 24 12:00:20 2022 auto update

---
+++ db_csv.py
@@ -0,0 +1,16 @@
+import sqlite3
+import csv
+
+files = ['output']
+for file in files:
+    conn = sqlite3.connect('D:\\nlp_project_data\\'+file+'.db')
+    cur = conn.cursor()
+    cur.execute("select * from OUTPUTS")
+    texts = cur.fetchall()
+    with open('D:\\nlp_project_data\\'+file+'.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        for text in texts:
+            writer = csv.writer(csvfile)
+            writer.writerow([text[0], text[1]])
+    conn.commit()
+    conn.close()
+print('done')

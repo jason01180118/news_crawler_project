c23a5047940e9c2ef140484f28fe3f6d98b71acd
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-01-01 19:38:44 +0800

    feat:Sun Jan  1 19:38:42 2023 auto update

--- output_data_bm25.py
+++ output_data_bm25.py
@@ -12,16 +12,15 @@ for values in datas.values():
     for key in values.keys():
         tags.append(key)
 
-df = pd.read_csv('D:\\nlp_project_data\\ESG_daily_news.csv')
+df = pd.read_csv('D:\\nlp_project_data\\output.csv', names=['text', 'tag'])
 
 sentences = []
 results = []
 
 for text in df['text'].fillna("").tolist():
-    for sentence in sent_tokenize(text):
-        if(len(sentence.split())) > 10:
-            sentences.append(sentence.split())
-            results.append(sentence)
+    if len(text.split()) >= 10:
+        sentences.append(text.split())
+        results.append(text)
 
 sentence_score = np.zeros(
     (len(tags), len(sentences)))
@@ -44,10 +43,9 @@ for key, values in datas.items():
 
 
 data_text = pd.DataFrame(results, columns=['text'])
-data_tags = pd.DataFrame(sentence_score.T, columns=tags)
+data_tags = pd.DataFrame(sentence_score.reshape(np.shape(sentence_score)[
+                         1], np.shape(sentence_score)[0]), columns=tags)
 
 data = pd.merge(data_text, data_tags, left_index=True, right_index=True)
-
-
-data.to_csv('D:\\nlp_project_data\\ESG_daily_result_multiple.csv',
+data.to_csv('D:\\nlp_project_data\\output_result_multiple.csv',
             encoding='utf-8', index=False)
